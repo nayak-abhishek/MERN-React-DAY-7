f1f3313949f3e2a5421b05659545fd730c0b0e49
nayak-abhishek <abhisheknayak01234@gmail.com>
2024-04-10 14:45:09 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Languages from './component/Languages';
+import Message from './component/Message';
+
+function App() {
+  
+
+  return (
+    <>
+    <Message />
+    <Languages/>
+    </>
+  );
+}
+
+export default App
---
+++ src/component/Message.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Message() {
+    let name = "REACT JS";
+  return (
+    <div>
+        <h1>Welcome to {name}</h1>
+    </div>
+  );
+}
+
+export default Message
